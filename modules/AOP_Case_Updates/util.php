62e8cea9b0bc36286d4d2866132f5cda2a4f8c73
Marc Sánchez <sanchezfauste@gmail.com>
2017-08-25 13:35:34 +0200

    Force aop_parse_template() to getBeans without using cache

--- modules/AOP_Case_Updates/util.php
+++ modules/AOP_Case_Updates/util.php
@@ -130,7 +130,7 @@ function aop_parse_template($string, $bean_arr)
 
     foreach ($bean_arr as $bean_name => $bean_id) {
 
-        $focus = BeanFactory::getBean($bean_name, $bean_id);
+        $focus = BeanFactory::getBean($bean_name, $bean_id, array(), true, false);
 
         if ($bean_name === 'Leads' || $bean_name === 'Prospects') {
             $bean_name = 'Contacts';
